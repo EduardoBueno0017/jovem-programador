fe1b7110f9511c3dee3078de88e7b55494ec6537
EduardoBueno0017 <128435849+EduardoBueno0017@users.noreply.github.com>
2023-03-30 19:15:43 -0300

    Delete Novo1.pas

--- Novo1.pas
+++
@@ -1,8 +0,0 @@
-Program Pzim ;
-Begin
-   writeln('O primeiro programa a gente nunca esquece');
-   writeln('Eduardo Bueno');
-   writeln('Rua Rui Barbosa 1735, Bairro Sumaré, Rio do sul');
-   writeln('CEP-89165513, Telefone 47989022031');
-   
-End.
