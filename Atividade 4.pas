5f07571f9156d436593024b315a698d3e4936252
EduardoBueno0017 <128435849+EduardoBueno0017@users.noreply.github.com>
2023-03-30 19:16:05 -0300

    Delete Atividade 4.pas

--- Atividade 4.pas
+++
@@ -1,9 +0,0 @@
-Program Pzim ;
-Begin
-  writeln ('ALUNO(@)    (NOTA)');
-  writeln ('*******)    (****)');
-  writeln ('Aline)      (9,0)');
-  writeln ('Mario)      (Dez)');
-  writeln ('Sergio)     (4,5)');
-  writeln ('Shirley0    (7,0)');
-End.
