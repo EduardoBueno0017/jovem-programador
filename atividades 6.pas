9953c14a4bdd1d890af2722f8bc035034f021806
EduardoBueno0017 <128435849+EduardoBueno0017@users.noreply.github.com>
2023-04-01 18:22:05 -0300

    Add files via upload

---
+++ atividades 6.pas
@@ -0,0 +1,12 @@
+Program Pzim ;
+var
+  peso, altura : real;
+Begin
+       writeln('Qual o peso');
+       readln(peso);
+       writeln('qual a altura');
+       readln(altura);
+      writeln('IMC é de');
+      writeln(peso/(altura*altura));  
+      
+End.
